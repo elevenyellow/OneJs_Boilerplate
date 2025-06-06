36adede9785b59f9b767220abba5248248260b88
cook1e-monster <carlos.silva@elevenyellow.com>
2025-06-06 23:24:28 +0200

    add admin

--- apps/admin/index.ts
+++
@@ -1,6 +0,0 @@
-import { BootstrapService, Server } from '@EyJs'
-
-const container = await BootstrapService.bootstrap(import.meta.url)
-
-const server = container.get(Server)
-server.setContainer(container).start(3000)
