541809ccecf67cb331a98628f76cc2e3dd5e6fc5
Carlos Silva <carlos.silva@elevenyellow.com>
2026-06-17 11:43:27 +0200

    fix startup

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,7 +1,7 @@
 import { AuthPlugin, ClerkStrategy } from '@OneJs/auth'
 import { logger, OneJs } from '@OneJs/core'
 import { AutoLoaderPlugin, BootstrapLoader } from '@OneJs/core/bootstrap'
-import { EventBusPlugin, RedisBridge } from '@OneJs/event-bus'
+import { EventBusPlugin } from '@OneJs/event-bus'
 import { type AnyMiddleware, Server, ServerPlugin } from '@OneJs/server'
 import cors from '@elysiajs/cors'
 
@@ -10,7 +10,7 @@ const container = await new OneJs()
   .use(new BootstrapLoader())
   .use(new AuthPlugin(ClerkStrategy))
   .use(new ServerPlugin())
-  .use(new EventBusPlugin(new RedisBridge()))
+  .use(new EventBusPlugin())
   .start()
 
 const port = Number(process.env.PORT ?? 4000)
